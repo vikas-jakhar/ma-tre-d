5a4ed262f8912e3a3e18eb73147778d200c2502b
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-13 15:48:51 +0530

    slick slider update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,8 @@ AOS.init();
 $('.slider').slick({
     dots: false,
     infinite: true,
-    // prevArrow: ".prev",
-    // nextArrow: ".next",
+    prevArrow: ".prev",
+    nextArrow: ".next",
     speed: 300,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -28,3 +28,10 @@ $('.slider').slick({
         }
     ]
 });
+
+const switch12 = document.querySelector(".switch");
+const sliderbtn = document.querySelector(".slider-btn");
+
+switch12.addEventListener("click", function () {
+    sliderbtn.classList.toggle("btn-right");
+})
