a547b9beb41d115c4fddf3b808dfd9d0bc8accbe
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-04-15 17:07:49 +0530

    menu icon update

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,7 +49,7 @@ menuicon.addEventListener("click", function () {
 });
 
 $(document).ready(function () {
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+    $('.menu-icon').click(function () {
         $(this).toggleClass('open');
     });
 });
